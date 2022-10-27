7922a22339bb643ba4f051a7123edd86d1d78890
ignamartinoli <103866722+ignamartinoli@users.noreply.github.com>
2022-10-27 13:19:58 -0300

    fzf

--- init.lua
+++ init.lua
@@ -255,24 +255,13 @@ return require 'packer'.startup(function ()
 
 			require 'telescope'.setup {
 				extensions = {
-					fzf = { fuzzy = true, override_generic_sorter = true, override_file_sorter = true, case_mode = 'smart_case' },
 					['ui-select'] = { require 'telescope.themes'.get_dropdown() }
-				},
-				vimgrep_argument = { 'rg', '--smart-case' }
+				}
 			}
 
-			require 'telescope'.load_extension 'fzf'
 			require 'telescope'.load_extension 'ui-select'
 		end,
-		requires = {
-			'nvim-lua/plenary.nvim',
-			{ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-			'nvim-telescope/telescope-ui-select.nvim'
-		},
-		setup = function ()
-			vim.keymap.set('n', '<C-f>', function () require 'telescope.builtin'.live_grep() end)
-			vim.keymap.set('n', '<C-c>', function () require 'telescope'.load_extension('todo-comments').todo() end)
-		end
+		requires = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-ui-select.nvim' }
 	}
 
 	-- Colorscheme
